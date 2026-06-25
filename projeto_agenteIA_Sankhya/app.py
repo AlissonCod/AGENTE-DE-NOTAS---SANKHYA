359734ab6adf7758550f1e240291d40d32cb9924
Alisson Junior <108632057+AlissonCod@users.noreply.github.com>
2026-06-25 19:14:37 +0000

    melhoria design

--- projeto_agenteIA_Sankhya/app.py
+++ projeto_agenteIA_Sankhya/app.py
@@ -200,4 +200,4 @@ def exportar_lote():
         return jsonify({"erro": f"Erro interno ao gerar o arquivo Excel: {e}"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
